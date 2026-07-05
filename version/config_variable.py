a9f42ec15d3454a92f8f5bfaae9619307c8d9d5c
Nicray Teams <nicrayt@gmail.com>
2026-07-05 18:51:42 +0200

    Update config_variable.py

--- version/config_variable.py
+++ version/config_variable.py
@@ -3,10 +3,10 @@ base_pkg_filename:str = "base.json"
 base_upgrade_link:str = "https://pypkg-manager.vercel.app/up.json"
 save_pkg_dir:str = "pkg/"
 name_of_package_manager:str = " Onyx - PyPKG Manager "
-version_of_pkg_manager:str = "0.3.5"
+version_of_pkg_manager:str = "0.3.501"
 
 change_log:str = f"""The current version is v{version_of_pkg_manager} and the name of this version is{name_of_package_manager}
-Objectif pour la Version 0.3.5:
+Ver 0.3.5:
 - Enhanced security
 - Fewer bugs
 - Reinstall the program if it is broken using `upgrade.py -f "URL"`
